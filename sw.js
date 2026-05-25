afecc39dbd3b8e039c910e9ad1b00f0b08b83ba3
nanfuxueai <3085904871@qq.com>
2026-05-25 19:03:49 +0800

    🌙 睡眠监测助手 v1.0

---
+++ sw.js
@@ -0,0 +1,21 @@
+// 睡眠监测助手 - Service Worker
+const CACHE = "sleep-monitor-v1";
+const ASSETS = ["index.html", "manifest.json", "icons/icon-192.svg", "icons/icon-512.svg"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).catch(() => new Response("离线", { status: 503 })))
+  );
+});
